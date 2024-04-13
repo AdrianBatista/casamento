7ce3ef41091be5df324244d939cc5d7f89715731
Adrian Batista <adrianfnb@gmail.com>
2024-04-13 17:46:14 -0300

    add counter

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,6 +10,7 @@ export function HomePage() {
           width: "100vw",
           backgroundSize: "cover",
           backgroundPosition: "bottom right",
+          backgroundAttachment: "fixed",
           display: "flex",
           alignItems: "center",
         }}
@@ -38,6 +39,63 @@ export function HomePage() {
             16/11/2024
           </Typography>
         </Container>
+      </Box>
+      <Box sx={{ width: "100vw", height: "100vh", position: "relative" }}>
+        <Box
+          component={"img"}
+          src={"/public/moldura.png"}
+          sx={{
+            position: "absolute",
+            top: "20px",
+            left: "20px",
+            width: {
+              xs: "15rem",
+              md: "25rem",
+            },
+            zIndex: "10",
+          }}
+        />
+        <Box
+          component={"img"}
+          src={"/public/mancha.png"}
+          sx={{
+            position: "absolute",
+            bottom: "0",
+            left: "0",
+            width: {
+              xs: "15rem",
+              md: "25rem",
+            },
+          }}
+        />
+        <Box
+          component={"img"}
+          src={"/public/mancha.png"}
+          sx={{
+            position: "absolute",
+            top: "0",
+            right: "0",
+            width: {
+              xs: "15rem",
+              md: "25rem",
+            },
+            transform: "rotate(180deg)",
+          }}
+        />
+        <Box
+          component={"img"}
+          src={"/public/moldura.png"}
+          sx={{
+            position: "absolute",
+            bottom: "20px",
+            right: "20px",
+            width: {
+              xs: "15rem",
+              md: "25rem",
+            },
+            transform: "rotate(180deg)",
+          }}
+        />
       </Box>
     </Box>
   );
